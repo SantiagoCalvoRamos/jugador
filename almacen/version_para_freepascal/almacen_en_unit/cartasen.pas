dae22a05771656006a3c0ad0f853d5217bb59098
santiago <santiagocalvoramos@gmail.com>
2024-12-12 13:40:13 +0100

    modificados:     almacen/version_para_freepascal/almacen_en_unit/almacen.pas 	modificados:     almacen/version_para_freepascal/almacen_en_unit/cartasen.pas 	modificados:     almacen/version_para_freepascal/almacen_en_unit/consola.pas 	modificados:     almacen/version_para_freepascal/almacen_en_unit/jugador.pas

--- almacen/version_para_freepascal/almacen_en_unit/cartasen.pas
+++ almacen/version_para_freepascal/almacen_en_unit/cartasen.pas
@@ -1,3 +1,19 @@
+{
+Copyrigth (C) 2024 Santiago Calvo Ramos
+
+Este programa es software libre: puede redistribuirlo y/o modificarlo bajo los
+términos de la Licencia Pública General de GNU publicada por la
+Free Software Foundation, ya sea la versión 3 de la Licencia
+o (a su elección) cualquier versión posterior.
+
+Este programa se distribuye con la esperanza de que sea útil, pero SIN
+NINGUNA GARANTÍA; ni siquiera la garantía implícita de COMERCIABILIDAD o
+IDONEIDAD PARA UN PROPÓSITO PARTICULAR. Consulte la Licencia
+Pública General de GNU para obtener más detalles.
+
+Debería haber recibido una copia de la Licencia Pública General de
+GNU junto con este programa. Si no es así, consulte <https://www.gnu.org/licenses/>.
+}
 unit cartasen;
 
 interface
